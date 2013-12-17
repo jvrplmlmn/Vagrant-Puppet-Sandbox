438adda7ab6f384c809f988443ee5ea598d458de
Javier Palomo Almena <jvrplmlmn@gmail.com>
2013-12-17 16:38:26 +0100

    Fixed missplaced template

--- provision/modules/networking/manifests/init.pp
+++ provision/modules/networking/manifests/init.pp
@@ -4,7 +4,7 @@ class networking {
     owner   => 'root',
 	group   => 'root',
 	mode    => '0644',
-	content => template(networking/hosts.erb),
+	content => template('networking/hosts.erb'),
   }
 
 }
