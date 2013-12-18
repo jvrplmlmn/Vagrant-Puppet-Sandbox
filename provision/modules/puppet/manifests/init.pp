a3c79542bb4381fc78fad3357da482a4bb0f1e9f
Javier Palomo Almena <jvrplmlmn@gmail.com>
2013-12-18 20:54:40 +0100

    Puppet service (puppet agent/daemon) enabled

--- provision/modules/puppet/manifests/init.pp
+++ provision/modules/puppet/manifests/init.pp
@@ -1,10 +1,24 @@
+# Class: puppet
 #
+# This module installes and manages the Puppet client daemon
+# Parameters:
 #
+# Actions:
 #
-
+# Requires:
+#
+# Sample usage:
+#
+# [Remember: No empty lines between comments and class definition]
 class puppet {
 
 	package { 'puppet':
 		ensure => 'latest'
 	}
+	
+	service { 'puppet':
+		enable  => true,
+		ensure  => running,
+		require => Package['puppet'],
+    }
 }
